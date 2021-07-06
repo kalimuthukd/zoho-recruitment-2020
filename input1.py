279766248e4b5b25459edab689fd60276b8339de
kalimuthukd <86998527+kalimuthukd@users.noreply.github.com>
2021-07-06 10:24:44 +0530

    Update input1.py

--- input1.py
+++ input1.py
@@ -1,1 +1,14 @@
 # Python Program for input1
+
+entered_text = input ("Enter the text :")
+print(entered_text)
+print(int(len(entered_text)/2))
+count=int(len(entered_text)/2)
+
+
+for i in range(11):
+    if i == int(len(entered_text)/2):
+        for j in range(i):
+            count=i+j
+            print (entered_text[count] ,end=' ')
+    print()
